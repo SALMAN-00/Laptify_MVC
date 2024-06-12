884c99bda228a89dfd1e049877440205cb27919c
iplmr <45397969+iplmr@users.noreply.github.com>
2024-06-12 08:17:05 +0300

    Add project files.

---
+++ Laptify_MVC/Models/Laptify.cs
@@ -0,0 +1,27 @@
+﻿namespace Laptify_MVC.Models
+{
+    public class Laptify
+    {
+        public int Id { get; set; }
+        public int CategoryId { get; set; }
+        public string Name { get; set; }
+        public string ShortDescription { get; set; }
+        public string LongDescription { get; set; }
+
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public float Price { get; set; }
+        public string Processor { get; set; }
+        public string RAM { get; set; }
+        public string Storage { get; set; }
+        public string GraphicsCard { get; set; }
+        public string DisplaySize { get; set; }
+        public string OperatingSystem { get; set; }
+        public string Color { get; set; }
+        public int Stock { get; set; }
+        public string Img { get; set; }
+
+
+
+    }
+}
